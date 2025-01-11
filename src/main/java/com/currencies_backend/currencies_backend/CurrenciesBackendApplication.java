e103577b358206d539c1ac0b86d086c2f33f2f6d
Damngz <da.munozgz@gmail.com>
2025-01-11 18:08:12 -0300

    base project

---
+++ src/main/java/com/currencies_backend/currencies_backend/CurrenciesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.currencies_backend.currencies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenciesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrenciesBackendApplication.class, args);
+	}
+
+}
